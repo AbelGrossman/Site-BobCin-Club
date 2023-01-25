c6e30995fc8561d52992b21871213eb52bc45c35
Antoine Quint <graouts@apple.com>
2023-01-25 09:25:55 +0100

    Now we are in a good position to fix our bug.
    First, let's think about our task here. We want our `switchToImage` function to hide
    the previously-shown section and show the new section. This function gets the `id` of
    the new section as its parameter. To be able to show the previous section, we should
    keep track of the `id` of the previous section, so the first thing we'll do is to add
    a global `selectedId` variable that will hold that state.
    
    So the goal of our function can be split in 3 tasks:
    
    1. hide the previous section (if it exists)
    2. show the new section
    3. keep track of the id of the newly-selected section
    
    It's important when programming to make few assumptions on being provided valid
    information. So we should also check that the `id` provided matches a valid section.
    
    It's also very important when programming to do as little work as possible! So we
    should also check that the newly-provided `id` is not the same as `selectedId`, since
    in that case we have nothing to do. This could easily happen if the user clicks on the
    same button multiple times.
    
    We will still improve this code but we have now accomplished what we set out to do.

--- Revival House/siteJS.js
+++ Revival House/siteJS.js
@@ -1,8 +1,34 @@
 
+let selectedId;
+
 function switchToImage(id) {
-    document.getElementById("imgtext").style.display="none";
-    if (document.getElementById(id).className != "visible") {
-        document.getElementById("imgtext").style.display="block";
-        document.getElementById(id).className = "visible";
-    }
+    // Nothing to do if we have already select this section.
+    if (id == selectedId)
+        return;
+
+    // Get a reference to the section we want to select next
+    // and exit early if it's not valid.
+    const newSection = document.getElementById(id);
+    if (!newSection)
+        return;
+
+    // Given we have a valid new section, we can remove the previous
+    // section, if it exists. It's perfectly valid that it may not
+    // exist in the event that we're selecting a section for the
+    // first time.
+    const previousSection = document.getElementById(selectedId);
+    if (previousSection)
+        previousSection.removeAttribute("class");
+
+    // Now make the new section visible.
+    newSection.className = "visible";
+
+    // Finally, we must keep track of the id for the newly-selected section.
+    selectedId = id;
+}
+
+// Main function called upon site load
+
+function main() {
+    switchToImage('BS1img');
 }
