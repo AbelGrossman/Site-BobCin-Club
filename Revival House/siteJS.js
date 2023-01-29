d83e5dbdce47ceaa92c0a0c6db7988a7343b6045
Abel Grossman <123425704+AbelGrossman@users.noreply.github.com>
2023-01-29 17:19:50 +0100

    Update siteJS.js
    Added missing brackets.

--- Revival House/siteJS.js
+++ Revival House/siteJS.js
@@ -5,22 +5,22 @@ var selectedId;
 
 function switchToImage(id) {
     // Nothing to do if we have already select this section.
-    if (id == selectedId)
-        return;
+    if (id == selectedId){
+        return;}
 
     // Get a reference to the section we want to select next
     // and exit early if it's not valid.
     const newSection = document.getElementById(id);
-    if (!newSection)
-        return;
+    if (!newSection){
+        return;}
 
     // Given we have a valid new section, we can remove the previous
     // section, if it exists. It's perfectly valid that it may not
     // exist in the event that we're selecting a section for the
     // first time.
     const previousSection = document.getElementById(selectedId);
-    if (previousSection)
-        previousSection.classList.remove(visibleClass);
+    if (previousSection){
+        previousSection.classList.remove(visibleClass);}
 
     // Now make the new section visible.
     newSection.classList.add(visibleClass);
